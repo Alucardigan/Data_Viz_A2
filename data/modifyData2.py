5b9272339fcb25f1651986f6a8f162b3eb20e5a8
Alucardigan <thejas.t.vinod@gmail.com>
2024-10-05 09:13:58 +1000

    data manipulations

--- data/modifyData2.py
+++ data/modifyData2.py
@@ -13,20 +13,28 @@ stateNames = {
     "SA":"South Australia"
 
 }
-
+#housing approvals 
 df = pd.read_excel('housing_approvals.xlsx',sheet_name='Sheet1')
 df = pd.melt(df,id_vars=['Date'],var_name='State',value_name='Housing Approvals')
 df['Year'] = df["Date"].dt.year
 df = df.groupby(['Year','State'])['Housing Approvals'].mean().round().reset_index()
 df['Housing Approvals YoY Growth (%)'] = df.groupby('State')['Housing Approvals'].pct_change() * 100
 df.to_csv("HousingApprovalsByState.csv",index=False)
-
+#pop data
+#Pop Data
+popDf = pd.read_excel("ausPopData.xlsx", sheet_name="Sheet1")
+pivotPopDf = pd.melt(popDf,id_vars=['Date'],var_name='State',value_name='Population')
+print(pivotPopDf.head())
+pivotPopDf['Year'] = pivotPopDf["Date"].dt.year
+popDf = pivotPopDf.groupby(['Year','State'])['Population'].mean().reset_index()
+popDf.to_csv("popData.csv",index=False)
 df = pd.read_csv('popData.csv')
 df['Pop YOY Growth (%)'] = df.groupby('State')['Population'].pct_change() * 100
 df.to_csv("popData.csv",index=False)
 pop_df = pd.read_csv("popData.csv")
 housing_df = pd.read_csv("HousingApprovalsByState.csv")
-combined_df = pd.merge(pop_df, housing_df, on=["Year", "State"], how="inner")
+combined_df = pd.merge(pop_df, housing_df, on=["Year", "State"], how="outer")
 combined_df.dropna(inplace=True)
-combined_df.head()
+print(combined_df.head(18))
+
 combined_df.to_csv("HousingApprovalsVSPopulation.csv",index=False)
