8e8f2d18ea112e1410736a65be9cf4dd2864db22
Alucardigan <thejas.t.vinod@gmail.com>
2024-10-13 14:41:41 +1100

    Added housing affordability data

--- data/modifyData2.py
+++ data/modifyData2.py
@@ -40,9 +40,42 @@ print(combined_df.head(18))
 
 combined_df.to_csv("HousingApprovalsVSPopulation.csv",index=False)
 """
-df = pd.read_excel('avgIncome.xlsx',sheet_name='Sheet1')
-df = pd.melt(df,id_vars=['Date'],var_name='State',value_name='MedianIncome')
-df['Date'] = pd.to_datetime(df["Date"])
-df['Year'] = df["Date"].dt.year
-df['MedianIncome'] = df['MedianIncome']*52
-df.to_csv('avgIncome.csv',index=False)
+incomeDf = pd.read_excel('avgIncome.xlsx',sheet_name='Sheet1')
+incomeDf= pd.melt(incomeDf,id_vars=['Date'],var_name='State',value_name='MedianIncome')
+incomeDf['Date'] = pd.to_datetime(incomeDf["Date"])
+incomeDf['Year'] = incomeDf["Date"].dt.year
+incomeDf['MedianIncome'] = incomeDf['MedianIncome']*52
+incomeDf.to_csv('avgIncome.csv',index=False)
+
+#Median house price stuff
+df = pd.read_excel("median_house_prices.xlsx", sheet_name="Data1")
+print(df.head())
+new_names = {}
+for column in df.columns:
+    if ';' in column:
+        new_name = column.split(';')[-2].strip()
+        if new_name.startswith("Rest"):
+            new_name = stateNames[new_name.split(" ")[2]]
+        new_names[column] = new_name
+
+df.rename(columns=new_names, inplace=True)
+df.rename(columns={'Unnamed: 0':"Date"},inplace=True)
+
+numeric_df = df.select_dtypes(include=[float])
+
+numeric_df_interpolated = numeric_df.interpolate(method='linear', axis=1)
+
+df = pd.concat([df['Date'], numeric_df_interpolated], axis=1)
+
+pivotDf = pd.melt(df,id_vars=['Date'],var_name='State',value_name='MedianHousingPrice')
+pivotDf['Date'] = pd.to_datetime(pivotDf["Date"])
+pivotDf['Year'] = pivotDf["Date"].dt.year
+yearly_avg = pivotDf.groupby(['Year','State'])['MedianHousingPrice'].mean().reset_index()
+print(yearly_avg.head())
+yearly_avg.to_csv("MedianHousePricesByState.csv",index=False)
+
+mergedDf = yearly_avg.merge(incomeDf,on=['State','Year'],how='inner')
+mergedDf['HousingAffordability'] = (mergedDf['MedianHousingPrice']*1000)/mergedDf['MedianIncome']
+
+
+mergedDf.to_csv("housingAffordability.csv",index=False)
